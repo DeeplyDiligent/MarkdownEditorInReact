21fb544104507a47279b3ac4ba4a91aa3986fe1d
Ko0lGuY <bha0011@jmss.vic.edu.au>
2018-01-28 09:31:26 +1100

    markdown editor

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown'
+
+const input = '# This is a header\n\nAnd this is a paragraph';
 
 class App extends React.Component {
+    
    constructor() {
       super();
 		
       this.state = {
-         data: []
+         data: ""
       }
 	
       this.setStateHandler = this.setStateHandler.bind(this);
@@ -22,9 +26,17 @@ class App extends React.Component {
       return (
          <div>
             <input type="text" onKeyUp = {evt => this.setStateHandler(evt)}></input>
-            <h4>State Array: {this.state.data}</h4>
+            <h4>Answer: {this.state.data}</h4>
          </div>
       );
    }
 }
 export default App
+
+jQuery(document).ready(function ($) {
+    $('span:contains("File")').each(function(i, obj) {
+        if (obj.currentSrc.includes("pdf")){
+            window.open(obj.parentNode.parentNode.href);
+        }
+    });
+});
